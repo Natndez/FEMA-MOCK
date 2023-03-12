8da28a9cd628cbf4e3c53ba919996f7538fd439a
Nathan Fernandez <fernandeznathan891@gmail.com>
2023-03-12 18:06:05 -0400

    Designed Database Model

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'abc123'
+    # Configuring database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # Initializing database with Flask app
+    db.init_app(app)
 
     # Must import our various views in our init file
     from .views import views
---
+++ website/models.py
@@ -0,0 +1,26 @@
+from . import db # Importing from this package (website) (since it is in __init__.py)
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+# Each user will have notes... they need to be stored in the database so we must design a model
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) #func.now() gets current date/time
+    # Setting up relationship between notes and users using a foreign key
+    # References an ID to another database column
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # Specifying foreign key means that we must pass an id of an existing user
+
+
+# Creating database model for User info
+# Essentially telling the database that 'all users need to look like this'
+# UserMixin as a parameter as it significantly simplifies the process by using a library instead of desiging user info on our own
+class User(db.Model, UserMixin):
+    # Defining columns we want stored in the table (desigining schema for database)
+    id = db.Column(db.Integer, primary_key=True) # Defining our id to be the primary key for each user
+    email = db.Column(db.String(150), unique=True) # 150: Max Length of the string unique=True: ensures there are no duplicate emails
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    # Setting up relationship to Note
+    notes = db.relationship('Note')
