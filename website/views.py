b3877d55914ea7e99e12d81951ffaff6c744bfdf
Nathan Fernandez <fernandeznathan891@gmail.com>
2023-03-12 23:12:27 -0400

    Can now add and remove notes(will switch to job assignments later on)

--- website/views.py
+++ website/views.py
@@ -1,11 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 # Defining that this file is the "blueprint" for our application
 views = Blueprint('views', __name__)
 
 # This function runs whenever we go to our '/' route in our URL
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
     return render_template("home.html", user=current_user) #renders home.html from our templates folder
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    data = json.loads(request.data)
+    noteId = data['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
